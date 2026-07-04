a437de5998dae4a1c799f25a3d7636e165283775
Ajanaku Iretiayomide Samuel <iretiayomideajanaku01@gmail.com>
2026-07-04 08:55:02 +0100

    Fixed indentation error

--- toriloshop/settings.py
+++ toriloshop/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'toriloshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-    DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
