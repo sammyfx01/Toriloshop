58a9dc71baedd566a46c0741dcf12bdf7fa31386
Ajanaku Iretiayomide Samuel <iretiayomideajanaku01@gmail.com>
2026-07-04 10:45:48 +0100

    Added admin creation URL

--- toriloshop/urls.py
+++ toriloshop/urls.py
@@ -1,19 +1,23 @@
-from django.conf import settings
-from django.conf.urls.static import static
-
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.db import connection
+
+# Function to create admin user
+def create_admin(request):
+    try:
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+            return HttpResponse("✅ Admin created!<br>Username: <b>admin</b><br>Password: <b>admin123</b><br><br><a href='/admin/'>Go to Admin</a>")
+        else:
+            return HttpResponse("✅ Admin already exists!<br><br><a href='/admin/'>Go to Admin</a>")
+    except Exception as e:
+        return HttpResponse(f"❌ Error: {e}")
 
 urlpatterns = [
+    path('create-admin/', create_admin, name='create_admin'),
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('', include('users.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
